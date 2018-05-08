482907492562e1be545562f2a4ba792ed87629fe
David Maarek <davidmaarek55@gmail.com>
2018-05-08 11:45:54 +0200

    Connexion de l'utilisateur après son inscription

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -3,6 +3,7 @@ import { IonicPage, LoadingController, NavController, NavParams, ToastController
 import { AuthProvider } from "../../providers/auth/auth";
 import { HomePage } from "../home/home";
 import { TabsPage } from "../tabs/tabs";
+import { Storage } from "@ionic/storage";
 
 /**
  * Generated class for the SignupPage page.
@@ -34,30 +35,67 @@ export class SignupPage {
     public loadingCtrl: LoadingController,
     public auth: AuthProvider,
     public toastCtrl: ToastController,
+    public storage: Storage,
   ) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SignupPage');
   }
 
+  // Fonction d'inscription de l'utilisateur
   doSignup(){
-    let loading = this.loadingCtrl.create({
-      content: 'Connexion en cours'
+    // Affichage de loder
+    let signupLoading = this.loadingCtrl.create({
+      content: 'Inscription en cours'
     });
-    loading.present();
+    signupLoading.present();
 
+    // API inscription
     this.auth.signup(this.signupData).then((result) =>{
       this.data = result;
-      console.log(this.data);
-      loading.dismiss();
-      this.navCtrl.setRoot(TabsPage);
+      signupLoading.dismiss();
+
+      const loginData = {
+        login : this.signupData.email,
+        password: this.signupData.plainPassword
+      };
+
+      let loginLoading = this.loadingCtrl.create({
+        content: 'Connexion en cours'
+      });
+      loginLoading.present();
+
+      // API connexion / génération token
+      this.auth.login(loginData).then((result) =>{
+        this.data = result;
+        console.log(this.data.value);
+        this.storage.set('token', {property: this.data.value})
+          .then(
+            () => {
+              console.log('Token stored');
+            },
+            error => console.error('Error storing token', error)
+          );
+        loginLoading.dismiss();
+        this.navCtrl.setRoot(HomePage);
+      }, (error) => {
+        let toast = this.toastCtrl.create({
+          message: error.error.message,
+          duration: 10000,
+          position: 'top'
+        });
+        loginLoading.dismiss();
+        toast.present();
+      });
+
+      this.navCtrl.setRoot(HomePage);
     }, (error) => {
       let toast = this.toastCtrl.create({
         message: error.error.message,
         duration: 10000,
         position: 'top'
       });
-      loading.dismiss();
+      signupLoading.dismiss();
       toast.present();
     });
   }
