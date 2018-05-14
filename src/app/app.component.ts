8281a99d64bb14811b9109bcb2c1fcb67a47b481
David Maarek <davidmaarek55@gmail.com>
2018-05-14 20:51:49 +0200

    Modifications check isvalidtoken

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, ViewChild } from '@angular/core';
-import { Nav, NavController, NavParams, Platform } from 'ionic-angular';
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -7,21 +7,19 @@ import { TabsPage } from "../pages/tabs/tabs";
 import { LoginPage } from "../pages/login/login";
 import { AuthProvider } from "../providers/auth/auth";
 import { GlobalProvider } from "../providers/global/global";
-import { HomePage } from "../pages/home/home";
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  @ViewChild(Nav) nav: Nav;
-  rootPage:any = LoginPage;
+  rootPage:any = TabsPage;
 
   constructor(
     platform: Platform,
     statusBar: StatusBar,
     splashScreen: SplashScreen,
     public auth: AuthProvider,
-    public global: GlobalProvider,
+    public global: GlobalProvider
   ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
@@ -30,7 +28,6 @@ export class MyApp {
       splashScreen.hide();
 
       this.auth.isTokenValid().then(() => {
-        this.nav.setRoot(HomePage);
       }, error => {
         console.log(error);
         this.rootPage = LoginPage;
